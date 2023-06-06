06d17918a41de27f36dd694b767ae5d49f354b27
miyeong <kmy9810@naver.com>
2023-06-06 18:10:58 +0900

    Feat: 모델 세팅 & pay앱 생성

--- Our_Liberation_Note/settings.py
+++ Our_Liberation_Note/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt.token_blacklist",
     "user",
     "diary",
+    'pay',
 ]
 
 MIDDLEWARE = [
--- diary/models.py
+++ diary/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.urls import reverse
+from user.models import User, Group
 
-# Create your models here.
+
+# 예지
+class Note(models.Model):
+    pass
+
+
+# 미영
+class PlanPage(models.Model):
+    pass
+
+
+class Memo(models.Model):
+    pass
+
+
+# 제건
+class PhotoPage(models.Model):
+    pass
+
+
+class Photo(models.Model):
+    pass
+
+
+class Comment(models.Model):
+    pass
+
+
+# 예린
+class Stamp(models.Model):
+    pass
+
