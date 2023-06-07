b39c2dbeb89a631702ec23106a3ab58eb1a7a0d4
Eunnylog <lllala0710@gmail.com>
2023-06-07 13:45:51 +0900

    Feat: 회원가입, 로그인

--- user/views.py
+++ user/views.py
@@ -1,13 +1,21 @@
 from rest_framework.views import APIView
+from rest_framework import status, permissions
+from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
-
+from user.serializers import LoginSerializer, SignUpSerializer
 
 class SignupView(APIView):
-    pass
+     def post(self, request):
+        serializer = SignUpSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message":"해방일지 합류 완료!"}, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class LoginView(TokenObtainPairView):
-    # serializer_class = LoginSerializer
+    serializer_class = LoginSerializer
     pass
 
 class GroupView(APIView):
