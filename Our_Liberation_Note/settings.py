b26ef4e92b265279e69a822b5429fe62c687ec7d
miyeong <kmy9810@naver.com>
2023-06-12 16:37:56 +0900

    Fix: migrations 오류 해결

--- Our_Liberation_Note/settings.py
+++ Our_Liberation_Note/settings.py
@@ -81,20 +81,15 @@ WSGI_APPLICATION = "Our_Liberation_Note.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-# DATABASES = mysettings.DATABASES
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASES = mysettings.DATABASES
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
