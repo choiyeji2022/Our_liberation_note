2e1d8a4c897799f73f55934c2b42dcf04b4f5ebc
Eunnylog <lllala0710@gmail.com>
2023-06-13 16:15:01 +0900

    Comment: 코드 정리

--- Our_Liberation_Note/settings.py
+++ Our_Liberation_Note/settings.py
@@ -17,6 +17,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
 
--- user/urls.py
+++ user/urls.py
@@ -17,18 +17,7 @@ urlpatterns = [
     path("group/<int:group_id>/", views.GroupDetailView.as_view(), name="group"),
     path("my-page/", views.MyPageView.as_view(), name="my_page"),
     path("map/", views.MapView.as_view(), name="map"),
-    # 카카오 로그인
-    # path('kakao/login/', views.kakao_login, name='kakao_login'),
-    # path('kakao/callback/', views.kakao_callback, name='kakao_callback'),
-    # path('kakao/login/finish/', views.KakaoLogin.as_view(), name='kakao_login_todjango'),
-    # # 구글 로그인
-    # path('google/login/', views.google_login, name='google_login'),
-    # path('google/callback/', views.google_callback, name='google_callback'),
-    # path('google/login/finish/', views.GoogleLogin.as_view(), name='google_login_todjango'),
-    # 네이버 로그인
-    # path('naver/login', views.naver_login, name='naver_login'),
-    # path('naver/callback/', views.naver_callback, name='naver_callback'),
-    # path('naver/login/finish/', views.NaverLogin.as_view(), name='naver_login_todjango'),
+    # 소셜 로그인
     path("social/", views.SocialUrlView.as_view(), name="social_login"),
     path("kakao/", views.KakaoLoginView.as_view(), name="kakao_login"),
     path("naver/", views.NaverLoginView.as_view(), name="naver_login"),
