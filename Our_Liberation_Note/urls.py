78c614c08b39f64790e92420308fa321820f70bf
miyeong <kmy9810@naver.com>
2023-06-14 11:48:18 +0900

    Feat: test url

--- Our_Liberation_Note/urls.py
+++ Our_Liberation_Note/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("user/", include("user.urls")),
     path("note/", include("diary.urls")),
-    pate("test/", include("diary.urls")),
+    path("test/", include("diary.urls")),
 ]
