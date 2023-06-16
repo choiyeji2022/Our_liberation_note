c792433dd73a5417181c628bb21f85ac18d22d9c
Yang ye lin <oky07031217@gmail.com>
2023-06-16 16:00:31 +0900

    Feat: Trash() 추가

--- diary/views.py
+++ diary/views.py
@@ -173,7 +173,37 @@ class TodoView(APIView):
 
 # 휴지통
 class Trash(APIView):
-    pass
+    def get(self, request):
+        # note = Note.objects.filter(user
+        # photo = 
+        pass
+        
+    def post(self, request, photo_id):
+        try:
+            stamp = Stamp.objects.get(id=photo_id)
+            serializer = StampSerializer(stamp, data=request.data)
+
+        except Stamp.DoesNotExist:
+            serializer = StampSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save(photo_id=photo_id, user_id=request.user.id)
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        if serializer.is_valid():
+            if stamp.status == "0":
+                stamp.status = "1"
+                serializer.save(photo_id=photo_id, user_id=request.user.id)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            elif stamp.status == "1":
+                stamp.status = "0"
+                serializer.save(photo_id=photo_id, user_id=request.user.id)
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 # 스탬프
