20cb00c2d4e96c47d174ea1a9eb3287952dd5f90
miyeong <kmy9810@naver.com>
2023-06-17 21:54:20 +0900

    Fix: tosspay username오류 수정

--- pay/urls.py
+++ pay/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('success', views.success),
+    path('success', views.success.as_view()),
     path('fail', views.fail),
     path('api/subscription/', views.check_subscription.as_view(), name='check_subscription'),
 ]
