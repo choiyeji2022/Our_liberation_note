3bdbcba01a1e396d7a507aa5ebbc4a2d84c2c3f6
Msgun7 <fgarr@naver.com>
2023-06-19 12:02:25 +0900

    Fix: 사진첩 urls.py 수정

--- diary/views.py
+++ diary/views.py
@@ -111,22 +111,22 @@ class DetailPhotoPageView(APIView):
 
 # 댓글
 class CommentView(APIView):
-    def get(self, request, comment_id):
-        comment = get_object_or_404(Comment, user=request.user, id=comment_id, status__in=[1,2])
+    def get(self, request, photo_id, comment_id):
+        comment = get_object_or_404(Comment, user=request.user, id=photo_id, id=comment_id, status__in=[1,2])
         serializer = CommentSerializer(comment)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def put(self, request, comment_id):
-        comment = get_object_or_404(Comment, user=request.user, id=comment_id, status__in=[1,2])
+    def put(self, request, photo_id, comment_id):
+        comment = get_object_or_404(Comment, user=request.user, id=photo_id, id=comment_id, status__in=[1,2])
         serializer = CommentSerializer(comment, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, comment_id):
+    def delete(self, request, photo_id ,comment_id):
         # permission_classes = [permissions.IsAuthenticated]
-        comment = get_object_or_404(Comment, user=request.user, id=comment_id, status__in=[1,2])
+        comment = get_object_or_404(Comment, user=request.user, id=photo_id, id=comment_id, status__in=[1,2])
         delete_comment = CommentSerializer(comment).data
         delete_comment['status'] = 3
         serializer = CommentSerializer(comment, data=delete_comment, partial=True)
