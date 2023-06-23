ff2d2c13958a0b8efe909e122f86c08c5bbbf74c
Yang ye lin <oky07031217@gmail.com>
2023-06-23 19:59:58 +0900

    Fix: CommentSerializer() 수정

--- diary/serializers.py
+++ diary/serializers.py
@@ -96,6 +96,8 @@ class DetailPhotoPageSerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    user = serializers.EmailField(source="user.email", read_only=True)
+    
     class Meta:
         model = Comment
         fields = "__all__"
