1056da6e98375d7cc630cf4ec1409d1de5f2f12d
Eunnylog <lllala0710@gmail.com>
2023-06-28 15:50:31 +0900

    Fix: 그룹 이름 예외처리

--- Our_Liberation_Note/settings.py
+++ Our_Liberation_Note/settings.py
@@ -113,13 +113,13 @@ WSGI_APPLICATION = "Our_Liberation_Note.wsgi.application"
 GUNICORN_TIMEOUT = 300
 
 
-# DATABASES = mysettings.DATABASES
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+DATABASES = mysettings.DATABASES
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 # 이미지 삽입
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
