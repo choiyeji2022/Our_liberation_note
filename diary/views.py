62d11fbb897fd60ec8a66a582172bc13c7468830
miyeong <kmy9810@naver.com>
2023-06-28 12:16:10 +0900

    Fix: image 삭제 오류 수정

--- diary/views.py
+++ diary/views.py
@@ -23,6 +23,7 @@ from .serializers import (
     PhotoPageSerializer,
     PlanSerializer,
     StampSerializer,
+    PatchPhotoPageSerializer,
 )
 
 
@@ -134,18 +135,14 @@ class DetailPhotoPageView(APIView):
         delete_photo = DetailPhotoPageSerializer(photo).data
         delete_photo["status"] = 3
 
-        # image_file_path = photo.image.path
-        # image_file_name = os.path.basename(image_file_path)
+        print(delete_photo)
 
-        # with open(image_file_path, "rb") as f:
-        #     image_data = f.read()
-
-        # delete_photo["image"] = SimpleUploadedFile(image_file_name, image_data)
-
-        serializer = PhotoPageSerializer(photo, data=delete_photo, partial=True)
-        if serializer.is_valid(raise_exception=True):
+        serializer = PatchPhotoPageSerializer(photo, data=delete_photo, partial=True)
+        if serializer.is_valid():
             serializer.save()
-
+        else:
+            print(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
